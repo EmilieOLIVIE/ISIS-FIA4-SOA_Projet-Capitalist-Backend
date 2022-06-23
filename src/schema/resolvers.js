a5005bd8b7af0e200c563950a8055779756bb0e5
EmilieOLIVIE <emilie.olivie@hotmail.fr>
2022-06-23 22:59:59 +0100

    comment

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -83,6 +83,7 @@ module.exports = {
         engagerManager(parent, args, context) {
             context = calcScore(context)
 
+            //Find manager in world
             let newManager = context.world.managers.find(element => element.name === args.name)
 
             if (context.world.money < newManager.seuil) throw new Error(ERRORS.INSUFFICIENT_MONEY)
@@ -105,6 +106,7 @@ module.exports = {
         acheterCashUpgrade(parent, args, context) {
             context = calcScore(context)
 
+            //Find cash upgrade in world
             let cashUpgrade = context.world.upgrades.find(upgrade => upgrade.name === args.name)
 
             if (context.world.money < cashUpgrade.seuil) throw new Error(ERRORS.INSUFFICIENT_MONEY)
@@ -125,6 +127,7 @@ module.exports = {
         acheterAngelUpgrade(parent, args, context) {
             context = calcScore(context)
 
+            //Find angel upgrade in world
             let angelUpgrade = context.world.angelupgrades.find(upgrade => upgrade.name === args.name)
 
             if (context.world.activeangels < angelUpgrade.seuil) throw new Error(ERRORS.INSUFFICIENT_MONEY)
@@ -136,7 +139,8 @@ module.exports = {
             //Update target characteristics
             context = updateProduct(context, angelUpgrade)
 
-            context.world.activeangels = context.world.activeangels - angelUpgrade.seuil
+            //Update world's active angels
+            context.world.activeangels -= angelUpgrade.seuil
 
             //Save world
             saveWorld(context)
@@ -145,6 +149,7 @@ module.exports = {
         resetWorld(parent, args, context) {
             context = calcScore(context)
 
+            //Calculate earned angels according to player's score
             let earnedAngels = Math.trunc(150 * Math.sqrt(context.world.score / Math.pow(10, 15)) - context.world.totalangels)
             let totalangels = context.world.totalangels + earnedAngels
             let score = context.world.score
@@ -152,7 +157,7 @@ module.exports = {
             //Create new world from template
             context.world = world
 
-            //Update world's total angels & score
+            //Update world's total angels & score according to previous game
             context.world.activeangels = earnedAngels
             context.world.totalangels = totalangels
             context.world.score = score
@@ -174,6 +179,9 @@ function calcGeometricSequenceNSum(a0, q, n) {
     return (a0 - getGeometricSequenceNTerm(a0, q, n) * q) / (1 - q)
 }
 
+/**
+ * Calculate score according to elapsed time
+ */
 function calcScore(context) {
     let world = context.world
     let products = context.world.products
@@ -212,7 +220,11 @@ function calcScore(context) {
     return context
 }
 
+/**
+ * Update product characteristics according to given upgrade
+ */
 function updateProduct(context, upgrade) {
+    //Update product characteristics according to boost characteristics
     function update(newProduct, upgrade) {
         if (upgrade.typeratio === Globals.typeratio.gain) newProduct.revenu *= upgrade.ratio
         else if (upgrade.typeratio === Globals.typeratio.vitesse) newProduct.vitesse = Math.round(newProduct.vitesse / upgrade.ratio)
@@ -220,11 +232,13 @@ function updateProduct(context, upgrade) {
         return newProduct
     }
 
+    //If upgrade's target is all, loop through each product and update it
     if (upgrade.idcible === Globals.all.id) {
         context.world.products.forEach(product => {
             product = update(product, upgrade)
         })
     } else {
+        //Else, find relevant product and update it
         let newProduct = context.world.products.find(prod => prod.id === upgrade.idcible)
         newProduct = update(newProduct, upgrade)
     }
